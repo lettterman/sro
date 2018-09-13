0fc43f35e0c7e144c40dc85fb68c455f84a37c81
lettterman <aliewsky@mail.ru>
2018-09-13 22:12:33 +0300

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,51 +1,17 @@
 <?php
-if(isset($_POST['submit']))
-{
-$name = $_POST['tel']; // Get Name value from HTML Form
-
-$to = "vlasov_anvar_t@mail.ru"; // You can change here your Email
-$subject = "'$name' has been sent a mail"; // This is your subject
-
-// HTML Message Starts here
-$message ="
-<html>
-<body>
-<table style='width:600px;'>
-<tbody>
-<tr>
-<td style='width:150px'><strong>tel: </strong></td>
-<td style='width:400px'>$name</td>
-</tr>
-</tbody>
-</table>
-</body>
-</html>
-";
-// HTML Message Ends here
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: Admin <vlasov_anvar_t@mail.ru>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
-
-if(mail($to,$subject,$message,$headers)){
-// Message if mail has been sent
-echo "<script>
-alert('Mail has been sent Successfully.');
-</script>";
-}
-
-else{
-// Message if mail has been not sent
-echo "<script>
-alert('EMAIL FAILED');
-</script>";
-}
-}
-?>
-
-setTimeout(function() {
-window.location.href = "http://www.ya.ru";
-}, 1);
+$fio = $_POST['tel'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("vlasov_anvar_t@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: vlasov_anvar_t@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
