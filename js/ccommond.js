5450e65209286f21d930e6e461cb5f1f7029c490
lettterman <aliewsky@mail.ru>
2018-09-13 08:55:35 +0300

    Add files via upload

---
+++ js/ccommond.js
@@ -0,0 +1,30 @@
+
+$(".owl-carousel").owlCarousel({
+                loop:true, //Зацикливаем слайдер
+                margin:20, //Отступ от элемента справа в 50px
+                nav:true, //Отключение навигации
+                autoplay:true, //Автозапуск слайдера
+                smartSpeed:1000, //Время движения слайда
+                autoplayTimeout:2000, //Время смены слайда
+                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                	0:{
+                		items:1
+                	},
+                	600:{
+                		items:2
+                	},
+                	1000:{
+                		items:3
+                	}
+                }
+            });
+
+var owl=$(".owl-carousel");
+	owl.owlCarousel();
+	//$(".next") - находим нашу кнопку
+	$(".next").click(function(){
+		owl.trigger("next.owl.carousel");
+	});
+	$(".prev").click(function(){
+		owl.trigger("prev.owl.carousel");
+	});
