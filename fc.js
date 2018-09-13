b8a95e7eee6e08e100a808ed2c67bdf412497099
lettterman <aliewsky@mail.ru>
2018-09-13 22:58:19 +0300

    Add files via upload

--- fc.js
+++ fc.js
@@ -1,11 +1,11 @@
 function onSuccess() {
         // remove this to avoid redirect
-        window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+        window.location = window.location.pathname + "?message=vlasov_anvar_t@mail.ru+Successfully+Sent%21&isError=0";
     }
 
     function onError(error) {
         // remove this to avoid redirect
-        window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+        window.location = window.location.pathname + "?message=vlasov_anvar_t@mail.ru+could+not+be+sent.&isError=1";
     }
 
     var sendButton = $("#" + form_id + " [name='send']");
@@ -33,4 +33,3 @@ function onSuccess() {
     $form.submit(function( event ) {
         event.preventDefault();
     });
-</script>
